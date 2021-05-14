2b425e7baef128eda6d94de2ea7a897e163d941b
Guillermo Samboni <guillermosamboni0912@gmail.com>
2021-05-13 21:08:36 -0500

    despliegue 4 de la aplicacion en heroku

--- ambientalismo/settings.py
+++ ambientalismo/settings.py
@@ -136,15 +136,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+#STATICFILES_DIRS = [
+#    BASE_DIR / "static",
+#]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT="media"
 
 
-#STATIC_ROOT="static"
+STATIC_ROOT="static"
 
 
 # Default primary key field type
